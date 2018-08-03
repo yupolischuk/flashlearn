86f5fb7024f5bd26e9ee1fb208cee9c42c1d0f7d
yupolischuk <yumuscletank@gmail.com>
2018-08-03 17:36:16 +0300

    Added flashcard entity

--- flashcard.py
+++ flashcard.py
@@ -21,13 +21,46 @@ class Flashcard(db.Model):
     # __table_args__ = (
     #     {'mysql_character_set': 'utf8mb4', 'mysql_collate': 'utf8mb4_unicode_520_ci'},
     # )
-    id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255), unique=True, nullable=False, primary_key=True)
-    answer = db.Column(db.String(255), unique=True, nullable=False, primary_key=True)
-
 
     def __repr__(self):
         return "<Question: {}>".format(self.question)
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    flashcards = None
+    if request.form:
+        try:
+            flashcard = Flashcard(question=request.form.get("question"))
+            db.session.add(flashcard)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add flashcard")
+            print(e)
+    flashcards = Flashcard.query.all()
+    return render_template("index.html", flashcards=flashcards)
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newquestion = request.form.get("newquestion")
+        oldquestion = request.form.get("oldquestion")
+        flashcard = Flashcard.query.filter_by(question=oldquestion).first()
+        flashcard.question = newquestion
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update flashcard question")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    question = request.form.get("question")
+    flashcard = Flashcard.query.filter_by(question=question).first()
+    db.session.delete(flashcard)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == "__main__":
