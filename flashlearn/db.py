3fbe32ddeee8343038b93cab3cefeb6ac179f871
yupolischuk <yumuscletank@gmail.com>
2018-08-20 17:54:48 +0300

    Added showing how many flashcards have decks in homepage

--- flashlearn/db.py
+++ flashlearn/db.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, MetaData
 file = open('./flashcard.cfg', 'r')
 str = file.read()
 arr = str.split("=")
-config = arr[1][2:48]
-
+config = arr[1][2:49]
 
 def connect():
     engine = create_engine(config, convert_unicode=True)
--- flashlearn/home.py
+++ flashlearn/home.py
@@ -11,12 +11,31 @@ engine = connect()
 
 @bp.route('/', methods=['GET'])
 def home():
+    # TODO think how to rewrite without nested dictionary
+    decks_obj = engine.execute('SELECT * FROM `deck`')
+    counted_cards_obj = engine.execute('SELECT deck_id as id, count(*) as count_card FROM `flashcard` GROUP BY deck_id')
 
-    decks = engine.execute('SELECT * FROM `deck`')
+    decks = db_obj_to_dict(decks_obj)
+    counted_cards = db_obj_to_dict(counted_cards_obj)
 
-    return render_template('home.html', decks=decks)
+    # add counted cards to all decks
+    for key, value in decks.items():
+        if key in counted_cards:
+            value['count_cards'] = counted_cards[key].get('count_card')
+        else:
+            value['count_cards'] = 0
+
+    return render_template('home.html', decks=decks.values())
 
 
+def db_obj_to_dict(db_obj):
+    # creating dictionary from SQLAlchemy object: keys is ids values in rows
+    res = {}
+    for row in db_obj:
+        row = dict(row)
+        res[row.get('id')] = row
+
+    return res
 
 
 
